77384f70668de5364353f7bf536cc8c492986a89
Tong Sun <suntong@cpan.org>
2023-04-25 12:25:07 -0400

    - [+] add PanicHandler to func-url/main.go

--- func-url/main.go
+++ func-url/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"runtime/debug"
 
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
@@ -16,8 +17,17 @@ func Hello(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "Hello, %s!\n", ctx.UserValue("name"))
 }
 
+func panicHandler(ctx *fasthttp.RequestCtx, p interface{}) {
+	fmt.Println("Recovered in panicHandler", p, string(debug.Stack()))
+
+	ctx.Response.Reset()
+	ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+}
+
 func main() {
 	r := router.New()
+	r.PanicHandler = panicHandler
+
 	r.GET("/", Index)
 	r.GET("/hello/{name}", Hello)
 
